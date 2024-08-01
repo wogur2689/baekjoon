ec77a8549ad264eebffa031ac399e061c4d177cc
길재혁 <81075170+wogur2689@users.noreply.github.com>
2024-08-02 01:46:43 +0900

    Create 9498.py

---
+++ 9498.py
@@ -0,0 +1,12 @@
+i = int(input(""))
+
+if(i < 101 and i >= 90):
+    print("A")
+elif(i < 90 and i >= 80):
+    print("B")
+elif(i < 80 and i >= 70):
+    print("C")
+elif(i < 70 and i >= 60):
+    print("D")
+else:
+    print("F")
